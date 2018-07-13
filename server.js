89fec7eaac1a34e1bd21f8665835a85e5b3c2f76
Leonardo BALLAND <balland.leonardo@baleo-it.com>
2018-07-13 10:07:19 +0200

    update server abs paths

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const server = express();
 const path = require("path");
 
-server.use(express.static("./dist/contacts"));
+server.use(express.static(path.resolve(__dirname, "dist/contacts")));
 server.get("/*", (req, res) => {
-  res.sendFile(path.join("./dist/contacts/index.html"));
+  res.sendFile(path.resolve(__dirname, "dist/contacts/index.html"));
 });
 
 server.listen(process.env.PORT || 3000);
